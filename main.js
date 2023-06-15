02ac2d922ce4611678b992c3028e902e761f98e9
IRUMVAEmmanuel1 <emmanuelirumva1@gmail.com>
2023-06-15 14:55:09 +0200

    game function

--- main.js
+++ main.js
@@ -22,4 +22,32 @@ function getComputerChoice() {
     return `You Lose! ${computerSelection} beats ${playerSelection}`;
   }
   
+  function game() {
+    let playerScore = 0;
+    let computerScore = 0;
+  
+    for (let round = 1; round <= 5; round++) {
+      const playerSelection = prompt("Enter your choice (Rock, Paper, or Scissors):");
+      const computerSelection = getComputerChoice();
+      const result = playRound(playerSelection, computerSelection);
+      console.log(result);
+  
+      if (result.includes("Win")) {
+        playerScore++;
+      } else if (result.includes("Lose")) {
+        computerScore++;
+      }
+    }
+  
+    if (playerScore > computerScore) {
+      console.log("You win the game!");
+    } else if (playerScore < computerScore) {
+      console.log("You lose the game!");
+    } else {
+      console.log("It's a tie game!");
+    }
+  }
+  
+  // Start the game
+  game();
   
