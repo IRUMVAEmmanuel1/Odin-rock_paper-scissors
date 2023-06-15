ab91aa1cfeabf205d1e0da70e501a09edafcb20f
IRUMVAEmmanuel1 <emmanuelirumva1@gmail.com>
2023-06-15 14:48:38 +0200

    play ground function

--- main.js
+++ main.js
@@ -4,4 +4,22 @@ function getComputerChoice() {
     return choices[randomIndex];
   }
   
- 
+  function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+  
+    if (playerSelection === computerSelection) {
+      return "It's a tie!";
+    }
+  
+    if (
+      (playerSelection === 'rock' && computerSelection === 'scissors') ||
+      (playerSelection === 'paper' && computerSelection === 'rock') ||
+      (playerSelection === 'scissors' && computerSelection === 'paper')
+    ) {
+      return `You Win! ${playerSelection} beats ${computerSelection}`;
+    }
+  
+    return `You Lose! ${computerSelection} beats ${playerSelection}`;
+  }
+  
+  
