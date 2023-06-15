cdbc8d913b80df0d49248ca84525b3c6a901e2d4
IRUMVAEmmanuel1 <emmanuelirumva1@gmail.com>
2023-06-15 14:17:54 +0200

    added files

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
-  function getComputerChoice(){
-    const choices = ['Rock','Paper','Scissors'];
-    const randomIndex =Math.floor(Math.random() * choices.length)
-    return choices1[randomIndex]
-  }
+function getComputerChoice(){
+    const choices = ['Paper', 'Rock', 'Scissor'];
+    const randomIndex= Math.floor(Math.random() * choices.length)
+
+    return choices[randomIndex]
+}
+console.lo(getComputerChoice());
