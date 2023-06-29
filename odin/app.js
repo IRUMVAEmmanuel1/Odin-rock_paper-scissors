41bfee3ec5c534e34a93080bb33d8b1b9db2886f
IRUMVAEmmanuel1 <emmanuelirumva1@gmail.com>
2023-06-29 21:51:06 +0200

    changes

--- odin/app.js
+++ odin/app.js
@@ -48,4 +48,4 @@ const playRound = (playerSelection, computerSelection) =>{
 
 const playerSelection = 'rock'
 const computerSelection = computerPlay()
-console.log(playRound(playerSelection, computerSelection)
+console.log(playRound(playerSelection, computerSelection))
